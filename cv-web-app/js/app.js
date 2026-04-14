50a73ac3b4081b4a2ec2ca3484e1ac7672c8dae7
bookman7705 <shawnk7705@gmail.com>
2026-04-14 08:27:53 -0400

    Update app.js

--- cv-web-app/js/app.js
+++ cv-web-app/js/app.js
@@ -1,5 +1,5 @@
 import { initCamera, getVideo } from './camera.js';
-import { initTracking, processTrackingFrame } from './tracking.js?v=tracking-fix-3';
+import { initTracking, processTrackingFrame } from './tracking.js?v=tracking-fix-4';
 import { drawTracking } from './visualization.js';
 import {
   initUI,
